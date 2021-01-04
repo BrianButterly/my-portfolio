020d1d09f6b7278c229c357c0d5a6b53a2e53ae0
Brian Butterly <brianbutterly@Brians-MacBook-Pro.local>
2021-01-04 16:34:52 -0600

    basic app without full info, done

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import about from "../about.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
-export default function Home() {
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
+
+export default function About() {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+      name,
+      bio,
+      "authorImage": image.asset->url
+    }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading...</div>;
+
   return (
-    <main>
-      <h1>About Me!</h1>
+    <main className="relative">
+      <img src={about} alt="Irish cliff" className="absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            className="rounded w-32 h-32 lg:w-64 h-64 mr-8"
+            alt={author.name}
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">
+              Hey there. I'm{" "}
+              <span classNAme="text-green-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projectId="hmjlwxn2"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
