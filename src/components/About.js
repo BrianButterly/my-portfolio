2c673baac11dae9813bb5763c37b9fdebd84acb9
Brian Butterly <brianbutterly@Brians-MacBook-Pro.local>
2021-01-11 13:55:17 -0600

    making site more responsive

--- src/components/About.js
+++ src/components/About.js
@@ -28,9 +28,9 @@ export default function About() {
   if (!author) return <div>Loading...</div>;
 
   return (
-    <main className="relative">
+    <main className="relative min-w-max">
       <img src={about} alt="Irish cliff" className="absolute w-full h-full" />
-      <div className="p-10 lg:pt-48 container mx-auto relative">
+      <div className="p-10 lg:pt-48 container mx-auto relative ">
         <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
           <img
             src={urlFor(author.authorImage).url()}
